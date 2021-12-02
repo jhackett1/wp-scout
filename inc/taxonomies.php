da44abc4fed1f2a34de597867b6067f44b30b991
jaye <hello@jayehackett.com>
2021-12-02 21:54:37 +0000

    custom menu for service categories

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -1,8 +1,15 @@
 <?php
 
+function add_drafts_admin_menu_item()
+{
+    add_menu_page(__('Service Categories'), __('Service Categories'), 'read', 'edit-tags.php?taxonomy=service_categories');
+}
+add_action('admin_menu', 'add_drafts_admin_menu_item');
+
+
 function wps_create_custom_taxonomies()
 {
-    register_taxonomy('service_categories', 'post', array(
+    register_taxonomy('service_categories', 'service', array(
         "labels" => array(
             "name" => "Service Categories",
             "singular_name" => "Service Category",
