d5dce58290ba388672ae9b1863ff24b711cc943a
jaye <hello@jayehackett.com>
2021-12-03 16:25:21 +0000

    add basic styles

--- views/list.php
+++ views/list.php
@@ -12,8 +12,6 @@ $location = $wp->query_vars["location"] ?? "";
 $categories = $wp->query_vars["cat"] ?? [];
 $page = $wp->query_vars["page"] ?? 1;
 
-var_dump($category_options);
-
 $res = wps_fetch_services($search, $location, $categories, $page, $perPage);
 
 $results = $res->content;
@@ -24,10 +22,10 @@ get_header();
 
 ?>
 
-<form class="wps-search-form" method="get">
+<form class="wps-search-form wide-max-width" method="get">
     <div>
         <label for="search">Search</label>
-        <input name="search" id="search" type="search" value="<?php echo $search ?>">
+        <input name="search" id="search" type="search" placeholder="eg. football" value="<?php echo $search ?>">
     </div>
     <div>
         <label for="location">Near</label>
@@ -36,40 +34,50 @@ get_header();
     <button>Search</button>
 </form>
 
-<aside>
-    <form method="get">
-        <fieldset>
-            <legend>Categories</legend>
+<div class="wps-results-wrapper wide-max-width">
 
-            <?php foreach ($top_level_category_options as $filter) : ?>
-                <?php wps_category_checkbox($filter->name, $filter->slug, $categories, $filter->term_id, $category_options); ?>
+    <aside>
+        <form class="wps-filter-form" method="get">
+            <fieldset>
+                <legend>Categories</legend>
+
+                <ul>
+                    <?php foreach ($top_level_category_options as $filter) : ?>
+                        <?php wps_category_checkbox($filter->name, $filter->slug, $categories, $filter->term_id, $category_options); ?>
+                    <?php endforeach; ?>
+                </ul>
+
+            </fieldset>
+
+            <button>Update results</button>
+        </form>
+    </aside>
+
+    <div>
+        <ul class="wps-results">
+            <?php foreach ($results as $result) : ?>
+                <li class="wps-results__item">
+                    <small><?php echo $result->organisation->name ?></small>
+                    <a href="<?php echo get_site_url() . "/services/{$result->id}" ?>">
+                        <h2><?php echo $result->name ?></h2>
+                    </a>
+                    <p><?php echo wp_trim_words($result->description, 20) ?></p>
+
+                    <?php if ($result->distance_away) : ?>
+                        <p><em><?php echo prettyDistanceAway($result->distance_away) ?></em></p>
+                    <?php endif; ?>
+
+                </li>
             <?php endforeach; ?>
+        </ul>
 
-        </fieldset>
+        <footer>
+            <?php wps_pagination(intval($page), intval($totalPages)) ?>
+            <p>Showing <?php echo ((intval($page) - 1) * $perPage) + 1 ?> to <?php echo (intval($page)) * count($results)  ?> of <?php echo $totalResults ?> services.</p>
+        </footer>
+    </div>
 
-        <button>Update results</button>
-    </form>
-</aside>
+</div>
 
-<ul class="wps-results">
-    <?php foreach ($results as $result) : ?>
-        <li class="wps-results__item">
-            <small><?php echo $result->organisation->name ?></small>
-            <a href="<?php echo get_site_url() . "/services/{$result->id}" ?>">
-                <h2><?php echo $result->name ?></h2>
-            </a>
-            <p><?php echo wp_trim_words($result->description, 20) ?></p>
-
-            <?php if ($result->distance_away) : ?>
-                <p><em><?php echo prettyDistanceAway($result->distance_away) ?></em></p>
-            <?php endif; ?>
-
-        </li>
-    <?php endforeach; ?>
-</ul>
-
-<p>Showing <?php echo ((intval($page) - 1) * $perPage) + 1 ?> to <?php echo (intval($page)) * count($results)  ?> of <?php echo $totalResults ?> services.</p>
-
-<?php wps_pagination(intval($page), intval($totalPages)) ?>
 
 <?php get_footer(); ?>
